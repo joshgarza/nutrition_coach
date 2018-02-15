0cea98eecb794fc607cc204596fee4b183c90ef5
Josh Garza <josh@sf-iron.com>
2018-02-14 20:16:18 -0800

    finish calories_generator module

--- Measurebot/calories_generator.py
+++ Measurebot/calories_generator.py
@@ -1,16 +1,16 @@
 from statistics import mean
-# from decimal import Decimal
 import numpy as np
 
 dates = np.array([1, 3, 7, 8, 10, 13, 15, 17, 20, 22, 24, 29, 31, 34, 35, 38, 41, 48, 50, 51, 56, 57], dtype=np.float64)
 weights = np.array([259.2, 258.2, 259, 258.8, 258.4, 257.8, 258.4, 257.4, 256.8, 257.4, 256, 254, 253.4, 253.6, 252.8, 251.4, 251.2, 249.6, 248.8, 248, 246.2, 245.4], dtype=np.float64)
 calories = np.array([3167.142857, 3167.142857, 3167.142857, 3167.142857, 3167.142857, 3167.142857, 3167.142857, 3167.142857, 3167.142857, 3167.142857, 3167.142857, 2480, 2480, 2480, 2480, 2480, 2480, 2480, 2480, 2480, 2480, 2480], dtype=np.float64)
 
-# takes dates, weights, calories
+# takes dates, weights, calories, "training_cycle"
 class MacrosGenerator():
     dates = [] #input
     weights = [] #input
     calories = [] #input
+    training_cycle = "" #input
     total_calories = 0 #accumulates during macro generation
     mean_calories = 0 #calculates during init
     calories_per_lb = 0 #calculates during init; function call to calc_cal_per_lb
@@ -20,9 +20,23 @@ class MacrosGenerator():
     fat = 0 #accumulates during macro generation
     bodyweight = 0 #calculates during init
     assignment = 0 #calculates during init; function call to macro_generator
+    actual_rate_of_change = 0
+    expected_rate_of_change = [] #based on training cycle
+    goal_rates = {
+        "bulk": 1.5,
+        "maintenance": 0,
+        "cut": -1.5
+    }
+    multipliers = {
+        "protein_mult": .8,
+        "carb_mult": 1,
+        "fat_mult": .3,
+        "min_protein_mult": .5,
+        "min_fat_mult": .1,
+        "max_protein_mult": 1.25
+        }
 
-
-    def __init__(self, dates, weights, calories):
+    def __init__(self, dates, weights, calories, training_cycle):
         self.dates = dates
         self.weights = weights
         self.calories = calories
@@ -44,10 +58,13 @@ class MacrosGenerator():
         mean_cal2 = mean(calorie_list2)
         
         # assignments
+        self.training_cycle = training_cycle
         self.mean_calories = mean_cal2
         self.bodyweight = mean(weight_list2)
         self.calories_per_lb = self.calc_cal_per_lb(slope1, slope2, mean_cal1, mean_cal2)
-        self.goal_calories = self.calc_goal_calories(slope2, self.mean_calories, self.calories_per_lb)
+        self.actual_rate_of_change = float(slope2)
+        self.expected_rate_of_change = self.set_rate_of_change(training_cycle)
+        self.goal_calories = self.calc_goal_calories(self.expected_rate_of_change, self.actual_rate_of_change, self.calories_per_lb, self.training_cycle)
         self.assignment = self.macro_generator(self.total_calories, self.goal_calories, self.bodyweight)
 
     def best_fit_slope(self, dates, weights):
@@ -59,26 +76,34 @@ class MacrosGenerator():
         calories_per_lb = (mean_cal1 - mean_cal2)/(slope1-slope2)
         return calories_per_lb
 
-    def calc_goal_calories(self, slope, mean_calories, calories_per_lb):
-        goal_calories = mean_calories
+    def set_rate_of_change(self, training_cycle):
+        if training_cycle == "bulk":
+            expected_rate_of_change = [1, 2]
+            return expected_rate_of_change
+        elif training_cycle == "maintenance":
+            expected_rate_of_change = [-.4, .4]
+            return expected_rate_of_change
+        elif training_cycle == "cut":
+            expected_rate_of_change = [-2, -1]
+            return expected_rate_of_change
+
+    def calc_goal_calories(self, expected_rate_of_change, actual_rate_of_change, calories_per_lb, training_cycle):
+        updated_rate_goal = self.goal_rates[str(training_cycle)]
+        
+        if expected_rate_of_change[0] <= actual_rate_of_change <= expected_rate_of_change[1]:
+            goal_calories = self.mean_calories
+        else:
+            rate_diff = (updated_rate_goal - actual_rate_of_change) * calories_per_lb
+            goal_calories = self.mean_calories + rate_diff
         return goal_calories
 
     def macro_generator(self, total_calories, goal_calories, bodyweight):
-        multipliers = {
-        "protein_mult": .8,
-        "carb_mult": 1,
-        "fat_mult": .3,
-        "min_protein_mult": .5,
-        "min_fat_mult": .1,
-        "max_protein_mult": 1.25
-        }
-        
-        protein_mult = multipliers["protein_mult"]
-        carb_mult = multipliers["carb_mult"]
-        fat_mult = multipliers["fat_mult"]
-        min_protein_mult = multipliers["min_protein_mult"]
-        min_fat_mult = multipliers["min_fat_mult"]
-        max_protein_mult = multipliers["max_protein_mult"]
+        protein_mult = self.multipliers["protein_mult"]
+        carb_mult = self.multipliers["carb_mult"]
+        fat_mult = self.multipliers["fat_mult"]
+        min_protein_mult = self.multipliers["min_protein_mult"]
+        min_fat_mult = self.multipliers["min_fat_mult"]
+        max_protein_mult = self.multipliers["max_protein_mult"]
         
         while total_calories < goal_calories:
             # initial values get reduced or increased depending on total_calories relationship to goal_calories
@@ -111,40 +136,3 @@ class MacrosGenerator():
                 self.carbs = carbs
                 self.fat = fat
                 return int(protein), int(carbs), int(fat), int(total_calories)
-
-    # this needs to be reworked
-    # def calorie_generator() :
-    #   weekly_calories = mean(calories) * 7
-
-    #   # goal_calories = weekly_calories * expected_rate_of_change / 
-
-    #   if m <= .4 and m >= -.4:
-    #       print("You are maintaining your weight, with a slight change of ~" + str(m) + "lbs per week.")
-    #       if cycle == "bulk":
-    #           weekly_calories = weekly_calories * 1.14
-    #           print("To gain weight, eat " + str(weekly_calories/7) + " per day.")
-    #       elif cycle == "cut":
-    #           weekly_calories = weekly_calories * .86
-    #           print("To lose weight, eat " + str(weekly_calories/7) + " per day.")
-    #       else:
-    #           print("To continue maintaining, eat " + str(weekly_calories/7) + " per day.")
-    #   if m > .4:
-    #       print("You are gaining weight, with a change of ~" + str(m) + "lbs per week.")
-    #       if cycle == "maintenance":
-    #           weekly_calories = weekly_calories * .86
-    #           print("To maintain weight, eat " + str(weekly_calories/7) + " per day.")
-    #       elif cycle == "cut":
-    #           weekly_calories = weekly_calories * .7
-    #           print("To lose weight, eat " + str(weekly_calories/7) + " per day.")
-    #       else:
-    #           print("To continue gaining, eat " + str(weekly_calories/7) + " per day.")
-    #   if m < -.4:
-    #       print("You are losing weight, with a change of ~" + str(m) + "lbs per week.")
-    #       if cycle == "bulk":
-    #           weekly_calories = weekly_calories * 1.24
-    #           print("To gain weight, eat " + str(weekly_calories/7) + " per day.")
-    #       elif cycle == "maintenance":
-    #           weekly_calories = weekly_calories * 1.14
-    #           print("To gain weight, eat " + str(weekly_calories/7) + " per day.")
-    #       else:
-    #           print("To continue losing, eat " + str(weekly_calories/7) + " per day.")
